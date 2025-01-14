e42bdc2be44169426e17645325be955117b3b316
aglaem <aglae.michot@gmail.com>
2025-01-14 10:28:57 +0400

    header ok

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: controllers/home_controller.php");
+exit;
+?>
---
+++ views/components/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/components.css">
+    <link rel="shortcut icon" href="../assets/img/Logo.png" type="image/webp">
+    <title>Les religieuses des Sacrés-Cœurs</title>
+</head>
+
+<body>
+    <header>
+        <div class="title">
+            <img src="../assets/img/Logo.png" alt="logo" class="logo">
+            <h1>Les religieuses des Sacrés-Cœurs</h1>
+        </div>
+
+        <nav>
+            <a href="index.php">Accueil</a>
+            <a href="index.php?page=about">À propos</a>
+            <a href="index.php?page=contact">Contact</a>
+
+        </nav>
+    </header>
